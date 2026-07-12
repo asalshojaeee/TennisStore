50f5f00e275782335c8e96d587dbc2fb1b53240b
Asal Shojaee <asal.shojaee.15@gmail.com>
2026-07-12 15:40:12 +0330

    feat(AdminPanel):fetch info about product

---
+++ frontend/src/helpers/productCategory.jsx
@@ -0,0 +1,40 @@
+
+
+
+import React from 'react'
+
+const productCategory = [
+    {
+        id: 1,
+        value: "racket",
+        lable: "Racket"
+    }, {
+        id: 2,
+        value: "menShoe",
+        lable: "MenShoe"
+    }, {
+        id: 3,
+        value: "womenShoe",
+        lable: "WomenShoe"
+    },
+    , {
+        id: 4,
+        value: "womenClothes",
+        lable: "WomenClothes"
+    },
+    , {
+        id: 5,
+        value: "menClothes",
+        lable: "MenClothes"
+    },
+    {
+        id: 6,
+        value: "ball",
+        lable: "Ball"
+    },
+
+]
+
+
+
+export default productCategory
--- frontend/src/pages/AdminPanel.jsx
+++ frontend/src/pages/AdminPanel.jsx
@@ -1,10 +1,16 @@
 
+import axios from 'axios'
 
 import { useSelector } from 'react-redux'
-import React, { use, useEffect } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import { Link, Outlet, useNavigate } from 'react-router-dom'
-
+import { IoCloudUploadOutline } from "react-icons/io5";
+import productCategory from '../helpers/productCategory'
 export const AdminPanel = () => {
+
+    const [data,setData]=useState({
+        
+    })
 
     const user = useSelector(state => state?.user.user)
 
@@ -18,18 +24,29 @@ export const AdminPanel = () => {
         }
     }, [user])
 
+    const handleOnChange = (e) => {
+        const { name, value } = e.target;
 
-    const handleSubmit = () => {
 
     }
+    const handleSubmit = async () => {
 
+
+        const responseData = await axios.post("http://localhost:3000/api/uploadproduct", data, {
+            withCredentials: true
+        })
+
+    }
+    const handleUploadProduct = () => {
+
+    }
 
 
     return (
 
         <>
 
-            <main className='flex gap-5'>
+            <main className='flex gap-5  bg-blue-100'>
                 <div className='h-screen w-50 bg-blue-300'>
                     <h1 className='text-white font-bold text-center p-5'>{user?.name}</h1>
                     <p className='text-green-100 text-center'>{user?.role}</p>
@@ -39,28 +56,57 @@ export const AdminPanel = () => {
                     </div>
 
                 </div>
-                <div>
-                    <form action="" className='flex flex-col'>
+                <div className='mt-10 flex gap-20 text-slate-400'>
+                    <form action="" className='flex flex-col gap-5 p-5' onSubmit={handleSubmit}>
                         <label htmlFor="productName">Name:</label>
-                        <input id="productName" type="text" placeholder="" />
+                        <input id="productName" className='bg-slate-100 opacity-55 h-10 rounded-sm w-100' type="text" placeholder="" />
 
 
 
                         <label htmlFor="category">Category:</label>
-                        <input id="category" type="text" placeholder="" />
+                        <select name="" id="category" className='text-slate-400'>
+
+
+                            <option>Select Category</option>
+                            {productCategory.map((pro, index) => {
+                                return (
+                                    <option key={index} value={pro.value}>{pro.lable}</option>
+                                )
+
+                            })}
+                        </select>
 
                         <label htmlFor="price">Price:</label>
-                        <input id="price" type="text" placeholder="" />
+                        <input id="price" className='bg-slate-100 opacity-55 h-10 rounded-sm w-100' type="text" placeholder="" />
 
                         <label htmlFor="brandName">BrandName:</label>
-                        <input id="brandName" type="text" placeholder="" />
+                        <input id="brandName" className='bg-slate-100 opacity-55 h-10 rounded-sm w-100' type="text" placeholder="" />
 
                         <label htmlFor="sellingPrice">SellingPrice:</label>
-                        <input id="sellingPrice" type="text" placeholder="" />
+                        <input id="sellingPrice" className='bg-slate-100 opacity-55 h-10 rounded-sm w-100' type="text" placeholder="" />
 
                         <label htmlFor="description">Description:</label>
-                        <textarea name="" id="description" cols='9' rows={7}></textarea>
-                        <input type="file" placeholder="" />
+                        <textarea name="" className='bg-slate-100 opacity-55 h-10 rounded-sm w-100' id="description" cols='9' rows={7}></textarea>
+
+
+                        <label htmlFor="uploadImageInput">
+
+                            <div className="p-2 cursor-pointer rounded h-32 w-full flex justify-center items-center">
+
+                                <div className="text-slate-500 flex justify-center items-center flex-col gap-2">
+                                    <span className="text-3xl">
+                                        <IoCloudUploadOutline className='text-blue-400' />
+                                    </span>
+                                    <p className="text-sm">Upload Product Image</p>
+                                    <input
+                                        onChange={handleUploadProduct}
+                                        type="file" id="uploadImageInput" className="hidden" />
+
+                                </div>
+
+                            </div>
+                        </label>
+
                     </form>
                 </div>
 
